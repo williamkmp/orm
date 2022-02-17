1882c57111e731100914247d250f9e8842e4bd5d
William Kurnia Mulyadi Putra <williamkurnia123@gmail.com>
2022-02-17 19:45:55 +0700

    first released version 1.0.1 on npm

--- bin/boilerplate.js
+++ bin/boilerplate.js
@@ -1,5 +1,5 @@
 export const schema_boilerplate = 
-`import { table, col } from "w-orm"
+`import { table, col } from "@williamkmp/w-orm"
 
 //DO NOT REMOVE OR CHANGE THIS CONST NAME but you can change the content
 export const DATABASE_CONFIG = {
